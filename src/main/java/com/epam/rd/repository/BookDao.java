f8ec63556023e1566b551e82a30ea0eeb01c2768
Harish_Kumar2 <harish_kumar2@epam.com>
2021-09-30 11:57:03 +0530

    Revert "Test code for BookServiceImpl getAllBooks should return all Books"
    This reverts commit e660449c1cde05530013c89576b10b10fa39f6b2.

--- src/main/java/com/epam/rd/repository/BookDao.java
+++
@@ -1,7 +0,0 @@
-package com.epam.rd.repository;
-
-import com.epam.rd.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookDao extends JpaRepository<Book, Integer> {
-}
--- src/main/java/com/epam/rd/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.epam.rd.service;
-
-import com.epam.rd.dto.BookDto;
-
-import java.util.List;
-
-public interface BookService {
-    List<BookDto> getAllUsers();
-}
